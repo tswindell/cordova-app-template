0168cfd8924243ea7070745e20f0aa631cccc1dd
Tom Swindell <t.swindell@rubyx.co.uk>
2018-01-22 12:37:16 +0000

    Initial commit
    Signed-off-by: Tom Swindell <t.swindell@rubyx.co.uk>

---
+++ template/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <md-app id="app">
+    <md-app-content>
+      <h1>Zipper Template</h1>
+      <router-view />
+    </md-app-content>
+  </md-app>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import "assets/material-icons.css"
+</style>
---
+++ template/src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page">
+    <h2>Demo Home</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+</style>
